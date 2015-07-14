ae40353cae0750598633b2354fd4a13f8cc7c598
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2015-07-14 06:52:56 +0300

    a bit more tests

--- test/test_dumb.rb
+++ test/test_dumb.rb
@@ -8,4 +8,34 @@ describe 'DumbConnection' do
     db.select(:test, 1).must_equal [[1, 'hello', [1,2]]]
     db.select(513, 2).must_equal [[2, 'world', [3,4]]]
   end
+
+  it "should select by pk" do
+    db.select(:test, 1).must_equal [[1, 'hello', [1,2]]]
+    db.select(513, 2).must_equal [[2, 'world', [3,4]]]
+  end
+
+  it "should select by secondary index" do
+    db.select(:test, "hello", index: 1).must_equal [[1, 'hello', [1,2]]]
+    db.select(:test, "world", index: 'name').must_equal [[2, 'world', [3,4]]]
+    db.select(:test, ["hello"], index: 1).must_equal [[1, 'hello', [1,2]]]
+  end
+
+  it "should insert" do
+    db.insert(:test, [3, "johny", [5,6]])
+    db.select(513, 3).must_equal [[3, "johny", [5,6]]]
+  end
+
+  it "should update" do
+  end
+
+  describe "with field names" do
+    before {
+      db.define_fields(:test, [[:id, :int], [:name, :str], [:point, :array]])
+    }
+    it "should select by hash" do
+      db.select(:test, {id: 1}).must_equal [{id:1, name:'hello', point:[1,2]}]
+      db.select(:test, {name: "world"}).must_equal [{id:2, name:'world', point:[3,4]}]
+    end
+
+  end
 end
