ead994371d3c0197c0e561422c75081edd53e7c8
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2018-08-19 20:04:41 +0300

    fixes in test/config.lua for newer tarantool

--- test/config.lua
+++ test/config.lua
@@ -1,18 +1,23 @@
 local log = require 'log'
 box.cfg{
     listen = 33013,
-    wal_dir='.',
-    snap_dir='.',
+    memtx_dir='.',
     wal_mode='none',
 }
 box.once('initbox', function()
 local s1 = box.schema.space.create('test', {id = 513, if_not_exists = true})
-local ip = s1:create_index('primary', {type = 'hash', parts = {1, 'NUM'}, if_not_exists = true})
+local ip = s1:create_index('primary', {type = 'hash', parts = {1, 'unsigned'}, if_not_exists = true})
 local iname = s1:create_index('name', {type = 'tree', parts = {2, 'STR'}, if_not_exists = true})
 local irtree = s1:create_index('point', {type = 'rtree', unique=false, parts = {3, 'ARRAY'}, if_not_exists = true})
-local ipname = s1:create_index('id_name', {type = 'tree', parts = {1, 'NUM', 2, 'STR'}})
+local ipname = s1:create_index('id_name', {type = 'tree', parts = {1, 'unsigned', 2, 'STR'}})
 local s2 = box.schema.space.create('test1', {id = 514, if_not_exists = true})
-local ip = s2:create_index('primary', {type = 'hash', parts = {1, 'NUM'}, if_not_exists = true})
+local ip = s2:create_index('primary', {type = 'hash', parts = {1, 'unsigned'}, if_not_exists = true})
+
+box.schema.user.create('tester', {password='testpass'})
+box.schema.user.grant('tester', 'read,write,execute', 'universe')
+box.schema.func.create('reseed')
+box.schema.func.create('func1')
+end)
 
 function reseed()
     local s1 = box.space.test
@@ -30,10 +35,6 @@ function func1(i)
 end
 
 box.schema.user.grant('guest', 'read,write,execute', 'universe')
-box.schema.user.create('tester', {password='testpass'})
-box.schema.user.grant('tester', 'read,write,execute', 'universe')
-end)
-
 local console = require 'console'
 console.listen '0.0.0.0:33015'
 
